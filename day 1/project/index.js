3de5438282c7b94aeddf4a4a40e1da8a7d616f17
legendofzer0 <98453046+legendofzer0@users.noreply.github.com>
2022-04-17 20:46:44 +0545

    branch-1

---
+++ day 1/project/index.js
@@ -0,0 +1,11 @@
+const name = document.querySelector("Name")
+const rollNumber = document.querySelector("rollNumber")
+const level = document.querySelector("level")
+const age = document.querySelector("age")
+const DOB = document.querySelector("DOB")
+const Major = document.querySelector("Major")
+const submit = document.querySelector("submit")
+    // for event listeners
+submit.addEventListener('submit', e => {
+    console.log("works")
+})
---
+++ day 1/project2/index.js
@@ -0,0 +1,18 @@
+const name = document.querySelector("#Name")
+const rollNumber = document.querySelector("#rollNumber")
+const level = document.querySelector("#level")
+const age = document.querySelector("#age")
+const DOB = document.querySelector("#DOB")
+const Major = document.querySelector("#Major")
+const submit = document.querySelector("#submit");
+// console.log(submit);
+// console.log(name);
+// console.log(rollNumber);
+// console.log(level);
+// console.log(age);
+// console.log(DOB);
+// console.log(Major);
+
+// !for event listeners
+
+addEventListener()
